e637a0a18725dc9cf6477fec75c1af97c42aaeb4
incomparable1992 <gaurav121991@yahoo.com>
2019-12-21 14:47:14 +0530

    complete project 1, 2

--- UserAuth/myapp/views.py
+++ UserAuth/myapp/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout as auth_logout, login as auth_login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
+@login_required(login_url='login')
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        messages.add_message(request, messages.INFO, 'Successfully User registered.')
+        return redirect('login')
+    else:
+        return render(request, 'signup.html')
+
+
+@login_required(login_url='login')
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            messages.add_message(request, messages.INFO, 'The username and/or password you specified are not correct.')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
